0587b58924feb471606adc0c4a45742c09fa268f
Alejandro <alejandro.arancibia.aranda@gmail.com>
2025-09-17 01:36:37 -0300

    feat: add function  and

---
+++ src/board.py
@@ -0,0 +1,19 @@
+from typing import List, Tuple
+
+Board = List[List[int]]
+EmptyPosition = Tuple[int, int, int]
+
+
+def create_board(size: int = 4) -> Board:
+    """Create a square board depending on the size."""
+    return [[0] * size for _ in range(size)]
+
+
+def empty_position(board: Board) -> List[EmptyPosition]:
+    """It goes through each row and column and
+    returns the empty positions (row, col, val) in a tuple."""
+    result = []
+    for idx_row, row in enumerate(board):
+        for idx_col, val in enumerate(row):
+            if val != 0:
+                result.append((idx_row, idx_col, val))
