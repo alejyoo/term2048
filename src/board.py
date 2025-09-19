c59af27ddafa02004bb80203c6a83e496a6a6159
Alejandro <alejandro.arancibia.aranda@gmail.com>
2025-09-19 00:38:26 -0300

    feat: adds add_random_tiles function

--- src/board.py
+++ src/board.py
@@ -1,3 +1,4 @@
+from random import choices, sample
 from typing import List, Tuple
 
 Board = List[List[int]]
@@ -18,3 +19,19 @@ def empty_position(board: Board) -> List[EmptyPositions]:
             if val == 0:
                 result.append((idx_row, idx_col))
     return result
+
+
+def add_random_tiles(board: Board, count: int = 1) -> bool:
+    """Add random tiles (2 or 4) to empty board positions."""
+    empty_pos = empty_position(board)
+
+    if len(empty_pos) < count:
+        return False
+
+    selected_positions = sample(empty_pos, count)
+
+    for row, col in selected_positions:
+        tile_value = choices([2, 4], weights=[8, 2])[0]
+        board[row][col] = tile_value
+
+    return True
