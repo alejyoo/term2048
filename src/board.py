5b3b46e179c02a08677e1ed99216cf67b7fcd1dc
Alejandro <alejandro.arancibia.aranda@gmail.com>
2025-09-19 00:31:53 -0300

    fix: ensures correct return in function that did not return a value

--- src/board.py
+++ src/board.py
@@ -1,7 +1,7 @@
 from typing import List, Tuple
 
 Board = List[List[int]]
-EmptyPositions = Tuple[int, int, int]
+EmptyPositions = Tuple[int, int]
 
 
 def create_board(size: int = 4) -> Board:
@@ -11,9 +11,10 @@ def create_board(size: int = 4) -> Board:
 
 def empty_position(board: Board) -> List[EmptyPositions]:
     """It goes through each row and column and
-    returns the empty positions (row, col, val) in a tuple."""
+    returns the empty positions (row, col) in a tuple."""
     result = []
     for idx_row, row in enumerate(board):
         for idx_col, val in enumerate(row):
             if val == 0:
-                result.append((idx_row, idx_col, val))
+                result.append((idx_row, idx_col))
+    return result
